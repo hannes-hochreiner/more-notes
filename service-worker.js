308dcc92c72d8f455288ce253fcb28000f90cb53
travis-ci <travis@travis-ci.org>
2016-12-30 18:43:18 +0000

    Lastest version on successful travis build 164 auto-pushed to gh-pages

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["icon.svg","9987dcb78bc3c933700e73070be1945f"],["icon_144x144.png","c44181c39939c9f9b3f9fff7a0b1f623"],["icon_192x192.png","45ea99100b7d47f224021f7f64b3fb61"],["icon_96x96.png","abe452a2801772eeb09ad942c44cfd1c"],["index.html","03aac0f868ba52ca95d5c9dd455c3d88"],["manifest.webmanifest","4595783d6418d6dcdbe3afc903a40421"],["static/css/main.3616147d.css","c28361a8632a0960aada42b0b65b4dff"],["static/css/main.3616147d.css.map","87ad0d7d6b894699dfd275e3f0165cb5"],["static/js/main.8ece8c44.js","791d3c91b08c9ff1a17ace872cb38996"]];
+var precacheConfig = [["icon.svg","9987dcb78bc3c933700e73070be1945f"],["icon_144x144.png","c44181c39939c9f9b3f9fff7a0b1f623"],["icon_192x192.png","45ea99100b7d47f224021f7f64b3fb61"],["icon_96x96.png","abe452a2801772eeb09ad942c44cfd1c"],["index.html","2f617ad5a520d31cfb97a13c9322357f"],["manifest.webmanifest","4595783d6418d6dcdbe3afc903a40421"],["static/css/main.3616147d.css","c28361a8632a0960aada42b0b65b4dff"],["static/css/main.3616147d.css.map","87ad0d7d6b894699dfd275e3f0165cb5"],["static/js/main.4c9504e1.js","740d70d2a98e96a9e2deea44a9831ded"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
